2cd97648132fec6e53d32ee11d875c0406bc2822
yassarrich <yassarrich@gmail.com>
2023-10-01 13:28:01 -0400

    template for login button

--- frontend/src/app/login/LoginButton.js
+++ frontend/src/app/login/LoginButton.js
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client'
+
+import React from "react";
 
 function LoginButton() {
     const handleLogin = () => {
---
+++ frontend/src/app/login/page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginButton from './LoginButton';
+
+const MyPage = () => {
+  return (
+    <div>
+      <LoginButton />
+    </div>
+  );
+};
+
+export default MyPage;
