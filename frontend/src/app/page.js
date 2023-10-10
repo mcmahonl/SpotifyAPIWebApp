02d60a82c47ce48b0f045649816220cd6e0a5054
rebeccaanestad <rba20a@fsu.edu>
2023-10-10 19:49:53 -0400

    Fixing the error in page.js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -67,7 +67,8 @@ export default function Home() {
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Explore your favorite genres and find new ones!
           </p>
-        </div>
+        
+        </a>
 
         {/* Artist button */}
         <a
