b0ab424a91e8327b9503b6917f255d92e3a129c4
rebeccaanestad <rba20a@fsu.edu>
2023-12-03 13:09:44 -0500

    fixing page.jsx

--- frontend/src/app/playlists/page.jsx
+++ frontend/src/app/playlists/page.jsx
@@ -4,21 +4,21 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 
 import useAuth from '../../hooks/useAuth';
-import { fetchTopTracks } from '../../actions/spotifyAPI';
-import TrackCard from '../../components/PlaylistCard';
+import { fetchPlaylists } from '../../actions/spotifyAPI';
+import PlaylistCard from '../../components/PlaylistCard';
 import * as styles from './page.module.css';
 
 const Playlists = () => {
     const token = useAuth();
     const router = useRouter();
   
-    const [tracks, setPlaylists] = useState([]);
+    const [playlists, setPlaylists] = useState([]);
   
-    // Fetch top tracks
+    // Fetch playlists
     useEffect(() => {
       if (token) {
-        fetchTopTracks({ token: token }).then((data) => {
-          setTracks(data);
+        fetchPlaylists({ token: token }).then((data) => {
+          setPlaylist(data);
           console.log(data);
         }).catch((error) => {
           // if the token is invalid, redirect user to auth page
@@ -32,13 +32,13 @@ const Playlists = () => {
       }
     }, [token]);
 
-    const trackCards = tracks.map((track, index) => {
+    const playlistCards = playlists.map((playlist, index) => {
         return (
-          <TrackCard track={track} index={index} />
+          <PlaylistCard playlist={playlist} index={index} />
         );
       });
     
-      if (token && !tracks) {
+      if (token && !playlists) {
         return (
           <main className={styles.wrapper}>
             <div className="flex flex-col items-center justify-center mt-32 mb-32">
@@ -55,16 +55,16 @@ const Playlists = () => {
       {/* Title */}
       <div className="flex flex-col items-center justify-center mt-16 mb-16">
         <h1 className="text-6xl font-bold text-center mb-2">
-          Your favorite <span className="text-green-500">tracks</span>.
+          Your favorite <span className="text-green-500">playlists</span>.
         </h1>
         <p className="text-xl text-center text-gray-500">
           Based on what you listen to the most.
         </p>
       </div>
 
-      {/* Track Cards */}
+      {/* Playlist Cards */}
       <ul className="flex flex-wrap justify-center">
-        {trackCards}
+        {playlistCards}
       </ul>
     </main>
   );
