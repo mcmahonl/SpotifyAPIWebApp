e83f90a4be74b059a11c88b54f124d0ffc0acf14
yassarrich <yassarrich@gmail.com>
2023-12-15 05:25:58 -0500

    Added styling to profile page

--- frontend/src/app/profile/page.jsx
+++ frontend/src/app/profile/page.jsx
@@ -52,24 +52,42 @@ const ProfilePage = () => {
   return (
     <main className={styles.wrapper}>
       <div className={styles.wrapper}>
-        <button className={styles.logoutButton} onClick={() => { logout(); window.location.href = "/"; }}>Logout</button>
-      </div>
-      <div>
-      <h2>{profile?.display_name}</h2>
-      <p>ID: {profile?.id}</p>
-      <p>Type: {profile?.type}</p>
-      <p>Followers: {profile?.followers.total}</p>
-      <p>
-        Spotify URL: <a href={profile?.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-          Open Spotify
-        </a>
-      </p>
-      <p>
-        Profile URL: <a href={profile?.href} target="_blank" rel="noopener noreferrer">Open Profile</a>
-      </p>
       
-        
-      </div>
+      <div class="flex items-center h-screen w-full justify-center">
+
+      <div className="max-w-xs">
+    <div className="bg-white shadow-xl rounded-lg py-3">
+        <div className="photo-wrapper p-2">
+            <img className="w-32 h-32 rounded-full mx-auto" src="https://picsum.photos/seed/picsum/200/300" alt="John Doe"></img>
+        </div>
+        <div className="p-2">
+            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">{profile?.display_name}</h3>
+            <div className="text-center text-gray-400 text-xs font-semibold">
+                <p>{profile?.followers.total}</p>
+            </div>
+            <table className="text-lg my-3">
+                <tbody><tr>
+                    <td className="px-2 py-2 text-gray-500">{profile?.id}</td>
+                </tr>
+              
+            </tbody></table>
+
+            <div className="text-center mb-3">
+            <p className="text-gray-900 pb-2 mb-2">
+            <a href={profile?.external_urls.spotify} target="_blank" rel="noopener noreferrer">
+              Open Spotify
+            </a>
+      </p>
+      <p className=""><button className={styles.logoutButton} onClick={() => { logout(); window.location.href = "/"; }}>Logout</button></p>
+            </div>
+            
+
+        </div>
+    </div>
+</div>
+</div>
+    
+     </div>
     </main>
   );
 }
